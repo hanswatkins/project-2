b4a4d3b3b50af20ce54a4d43a9ddd615cac973c7
hanswatkins <hanswatkins@gmail.com>
2022-07-21 21:34:54 -0700

    fixed mail icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import email from './email.png'
 
 const Contact = () => {
     return (
         <body className='contact-div'> 
             <div className="contact-div-2">
-                <a className="email" href="mailto:hanswatkins@gmail.com"><img src='https://cdn-icons.flaticon.com/png/512/2549/premium/2549872.png?token=exp=1658462420~hmac=58b01986435095a2f9d67b50ff5eb59c' alt="mail" width='50px' ></img></a>
+                <a className="email" href="mailto:hanswatkins@gmail.com"><img src={email} alt="mail icon" width='50px' ></img></a>
             </div>
         </body>
     );
