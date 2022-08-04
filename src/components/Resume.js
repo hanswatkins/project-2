9b657d682d5752ccc50e67dd4a5e0ddfaa82d0f0
hanswatkins <hanswatkins@gmail.com>
2022-08-04 09:26:13 -0700

    added links to resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,90 +2,123 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const Resume = () => {
-    return (
-        <div>
-            {/* You can view and download my resume <a href="https://docs.google.com/document/d/1Gx3JwOODc4qOFvkG9NsVxPdTrbw-mE0HobyCYdBzDIA/edit?usp=sharing" rel="noreferrer">here</a> */}
+  return (
+    <div>
+      {/* You can view and download my resume <a href="https://docs.google.com/document/d/1Gx3JwOODc4qOFvkG9NsVxPdTrbw-mE0HobyCYdBzDIA/edit?usp=sharing" rel="noreferrer">here</a> */}
 
-            {/* <iframe title='test' src="https://docs.google.com/document/d/e/2PACX-1vQX4GGWczXu2QjpcbhIc1gwgFT9YsM9XBbVBtRsJvPzANKEoR_LFiwEZKfqYE_No1_jxiNGHkKYBPah/pub?embedded=true"></iframe> */}
+      {/* <iframe title='test' src="https://docs.google.com/document/d/e/2PACX-1vQX4GGWczXu2QjpcbhIc1gwgFT9YsM9XBbVBtRsJvPzANKEoR_LFiwEZKfqYE_No1_jxiNGHkKYBPah/pub?embedded=true"></iframe> */}
 
-            
-<p className='resume'>
-                
-                    <h1>Hans Watkins | Front-End Web Developer</h1>
+      <p className='resume'>
+        <h1>Hans Watkins | Front-End Web Developer</h1>
 
-    <p>hanswatkins.com | 425-802-9812 | hanswatkins@gmail.com  | in/hanswatkins | github.com/hanswatkins</p>
+        <p>
+          <a href='https://www.hanswatkins.com'>hanswatkins.com</a> |{' '}
+          <a href='tel:425-802-9812'>425-802-9812</a> |{' '}
+          <a href='mailto:hanswatkins@gmail.com'>hanswatkins@gmail.com</a> |
+          <a href='www.linkedin/in/hanswatkins'>linkedin</a> |{' '}
+          <a href='https://www.github.com/hanswatkins' className='resume-links'>
+            github
+          </a>
+        </p>
 
+        <h2>RELEVANT EXPERIENCE</h2>
 
+        <h3>Space Battle | Solo</h3>
 
-    <h2>RELEVANT EXPERIENCE</h2>
+        <p>Developer | July 2022 | Live | Github</p>
 
-    <h3>Space Battle |  Solo</h3>                                    
+        <ul>
+          <li>Used only JavaScript to create a turn-based space battle game</li>
+          <li>
+            Styled console read-outs using CSS to create an interactive
+            experience for the user
+          </li>
+          <li>Third Thing</li>
+        </ul>
 
-    <p>Developer | July 2022 | Live | Github</p>                                                            
+        <h3>Random Cat Generator | Solo Project</h3>
 
-    <ul>
-        <li>Used only JavaScript to create a turn-based space battle game</li>
-        <li>Styled console read-outs using CSS to create an interactive experience for the user</li>
-        <li>Third Thing</li>
-    </ul>
+        <p>Developer | July 2022 | Live | Github</p>
 
-    <h3>Random Cat Generator |  Solo Project</h3>                                     
+        <ul>
+          <li>
+            Used The Cat API to create a website that generates random cat
+            images and search cat images by category
+          </li>
+          <li>
+            Used HTML, CSS, JavaScript, DOM, Events, APIs and AJAX to create the
+            website
+          </li>
+          <li>Deployed using GitHub Pages</li>
+        </ul>
 
-    <p>Developer | July 2022 | Live | Github</p>                                                           
+        <h3>Pixart | Solo Project</h3>
 
-    <ul>
-        <li>Used The Cat API to create a website that generates random cat images and search cat images by category</li>
-        <li>Used HTML, CSS, JavaScript, DOM, Events, APIs and AJAX to create the website</li>
-        <li>Deployed using GitHub Pages</li>
-    </ul>
+        <p>Developer | June 2022 | Live | Github</p>
 
-    <h3>Pixart |  Solo Project</h3>                                     
+        <ul>
+          <li>
+            Created a web application for drawing pixel art with user-chosen
+            colors
+          </li>
+          <li>
+            Used HTML and CSS to style the page and change the color of pixels
+            on mouse hover
+          </li>
+          <li>Deployed project using Netlify</li>
+        </ul>
 
-    <p>Developer | June 2022 | Live | Github</p>                                                            
+        <h2>WORK EXPERIENCE</h2>
 
-    <ul>
-        <li>Created a web application for drawing pixel art with user-chosen colors</li>  
-        <li>Used HTML and CSS to style the page and change the color of pixels on mouse hover</li>
-        <li>Deployed project using Netlify</li>  
-    </ul>
+        <h3>Music Producer</h3>
 
-    <h2>WORK EXPERIENCE</h2>
+        <p>Los Angeles 2017-2022</p>
+        <p>Arbour</p>
 
-    <h3>Music Producer</h3> 
+        <ul>
+          <li>
+            Collaborated with producers to deliver professional quality songs to
+            labels under strict deadlines & industry standards of audio fidelity
+          </li>
+          <li>
+            Amassed over 200,000,000 streams on my songs across Spotify and
+            Apple Music by creating promotional content on social media and
+            running paid ad campaigns based on listener data
+          </li>
+          <li>
+            Hired by Universal Music Group to star in a commercial for
+            professional studio monitor company Created music and video content
+            for Ableton and Bluenote Records
+          </li>
+        </ul>
 
-    <p>Los Angeles 2017-2022</p>
-    <p>Arbour</p>
+        <h2>TECHNICAL SKILLS</h2>
 
-    <ul>
-        
-        <li>Collaborated with producers to deliver professional quality songs to labels under strict deadlines & industry standards of audio fidelity</li>
-        <li>Amassed over 200,000,000 streams on my songs across Spotify and Apple Music by creating promotional content on social media and running paid ad campaigns based on listener data</li>
-        <li>Hired by Universal Music Group to star in a commercial for professional studio monitor company
-        Created music and video content for Ableton and Bluenote Records</li>
-    </ul>
+        <p>
+          HTML | JavaScript | CSS | SQL | Git | Github | Ruby | Python | Node |
+          Express.js{' '}
+        </p>
 
+        <h2>EDUCATION</h2>
 
+        <h3>General Assembly</h3>
+        <p>Los Angeles, CA</p>
 
-    <h2>TECHNICAL SKILLS</h2>
+        <p>Software Engineering Immersive August 2022</p>
 
-    <p>HTML | JavaScript | CSS | SQL | Git | Github | Ruby | Python | Node | Express.js </p>
+        <p>
+          Current student at General Assembly's Software Engineering Immersive
+          Program. I work with HTML, CSS, Javascript, React.js and APIs to build
+          interactive websites and web applications. I create and submit
+          software engineering deliverables with quick turnarounds.
+        </p>
 
-    <h2>EDUCATION</h2>
+        <p>Western Washington University | Bellingham, WA</p>
 
-    <h3>General Assembly</h3>                             
-    <p>Los Angeles, CA</p>
-
-    <p>Software Engineering Immersive August 2022</p>
-
-    <p>Current student at General Assembly's Software Engineering Immersive Program. I work with HTML, CSS, Javascript, React.js and APIs to build interactive websites and web applications. I create and submit software engineering deliverables with quick turnarounds.</p>
-
-    <p>Western Washington University     |    Bellingham, WA</p>
-
-    <p>Multidisciplinary Studies / Audio Engineering   |     June 2018</p>
-
-    </p>
-</div>
-    );
+        <p>Multidisciplinary Studies / Audio Engineering | June 2018</p>
+      </p>
+    </div>
+  );
 };
 
 export default Resume;
